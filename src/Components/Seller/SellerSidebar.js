f3c106ea51ddc6cac45f0c478f0726e13f5e5a29
rashed7865 <149649378+rashed7865@users.noreply.github.com>
2023-11-04 08:57:04 +0100

    Update SellerSidebar.js
    Email-admin

--- src/Components/Seller/SellerSidebar.js
+++ src/Components/Seller/SellerSidebar.js
@@ -1,54 +1,58 @@
-import React from 'react'
+import React from 'react';
 import { Menu } from 'antd';
 import Sider from 'antd/lib/layout/Sider';
 import { NavLink, useLocation } from 'react-router-dom';
-import { logout } from '../Auth/auth';
 
 export const SellerSidebar = () => {
-    const location = useLocation();
-    return (
-        <div className='sidebar adminSidebar'>
-            <Sider
-                style={{
-                    overflow: 'auto',
-                    height: '100vh',
-                }}
-            >
-                {/* <div><Link to='/' className='logo'>Dêrik-online-shop</Link></div> */}
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['/seller/products']} selectedKeys={[location.pathname]}>
-                    <Menu.Item key="/seller/products"><div className='sidebar-links'>
-                        <div>
-                            <i className="fas fa-chart-pie"></i>
-                        </div>
-                        <div>
-                            <NavLink to='/seller/products'>Products</NavLink>
-                        </div>
-                    </div>
-                    </Menu.Item>
-                    <Menu.Item key="/seller/orders">
-                        <div className='sidebar-links'>
-                            <div>
-                                <i className="fas fa-shopping-cart"></i>
-                            </div>
-                            <div>
-                                <NavLink to='/seller/orders'> Order Management</NavLink>
-                            </div>
-                        </div>
-                    </Menu.Item>
-                    <Menu.Item key="/seller/logout">
-                        <div className='sidebar-links'>
-                            <div>
-                                <i className="fas fa-sign-out-alt"></i>
-                            </div>
-                            <div>
-                                <a href='/login' onClick={() => { logout(() => { }) }}>
-                                    Logout
-                                </a>
-                            </div>
-                        </div>
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-        </div>
-    )
-}
+  const location = useLocation();
+
+  const adminEmail = 'rashedecomm728@gmail.com'; 
+
+  const handleContactAdmin = () => {
+    window.location.href = `mailto:${adminEmail}`;
+  };
+
+  return (
+    <div className='sidebar adminSidebar'>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+        }}
+      >
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['/seller/products']} selectedKeys={[location.pathname]}>
+          <Menu.Item key="/seller/products">
+            <div className='sidebar-links'>
+              <div>
+                <i className="fas fa-chart-pie"></i>
+              </div>
+              <div>
+                <NavLink to='/seller/products'>Products</NavLink>
+              </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item key="/seller/orders">
+            <div className='sidebar-links'>
+              <div>
+                <i className="fas fa-shopping-cart"></i>
+              </div>
+              <div>
+                <NavLink to='/seller/orders'>Order Management</NavLink>
+              </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item key="/seller/contact-admin">
+            <div className='sidebar-links'>
+              <div>
+                <i className="fas fa-envelope"></i> 
+              </div>
+              <div>
+                <button onClick={handleContactAdmin}>Kontakt Admin</button>
+              </div>
+            </div>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    </div>
+  );
+};
