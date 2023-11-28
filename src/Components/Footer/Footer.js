2fab85ab034c169c1027b4f33b133928596519d3
rashed <rashed0999229437@hotmail.com>
2023-11-28 12:10:40 +0100

    Revert "Revert "Revert "Revert "sprache""""
    This reverts commit 25a57588fa597df5edf4934d4f31f9b95463377a.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,12 @@ export const Footer = () => {
     return (
         <footer>
             <span>
-                Copyright © Dêrik-online-shop 2023
+                Copyright © Dêrik-online-shop 2023 
+               
+            </ span >
+            <span >
+               
+                Rashed Almahila
             </span>
         </footer>
     )
